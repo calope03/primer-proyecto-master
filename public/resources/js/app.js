92ea13a5c957ce8e9b73749163008708fa05f033
Carmen Lopez <carmenlop94@gmail.com>
2018-11-26 18:03:16 +0000

    add: funcion de busqueda

--- public/resources/js/app.js
+++ public/resources/js/app.js
@@ -1,71 +1,83 @@
-var identificadoresPais={
-  Mundial: 3155776842,
-  US: 1313621735, //USA
-  BR: 1111141961, //Brazil
-  DE: 1111143121, //Germany
-  FR: 1109890291, //France
-  GB: 1111142221, //UK
-  CA: 1652248171, //Canada
-  ZA: 1362528775, //SouthAfrica
-  YV: 1362527605, //Venezuela
-  UA: 1362526495, //Ukraine
-  TN: 1362525375, //Tunisia
-  TH: 1362524475, //Thailand
-  SV: 1362523615, //ElSalvador
-  SN: 1362523075, //Senegal
-  SI: 1362522355, //Slovenia
-  SA: 1362521285, //SaudiArabia
-  PY: 1362520135, //Paraguay
-  PT: 1362519755, //Portugal
-  PH: 1362518895, //Philippines
-  PE: 1362518525, //Peru
-  MY: 1362515675, //Malaysia
-  MA: 1362512715, //Morocco
-  LB: 1362511155, //Lebanon
-  KR: 1362510315, //SouthKorea
-  JP: 1362508955, //Japan
-  JO: 1362508765, //Jordan
-  JM: 1362508575, //Jamaic
-  HU: 1362506695, //Hungary
-  EG: 1362501615, //Egypt
-  EC: 1362501235, //Ecuador
-  DZ: 1362501015, //Algeria
-  BO: 1362495515, //Bolivia
-  AE: 1362491345, //UnitedArabEmirates
-  SG: 1313620765, //Singapore
-  SE: 1313620305, //Sweden
-  NO: 1313619885, //Norway
-  IE: 1313619455, //Ireland
-  DK: 1313618905, //Denmark
-  CR: 1313618455, //CostaRica
-  CH: 1313617925, //Switzerland
-  AU: 1313616925, //Australia
-  AT: 1313615765, //Austria
-  AR: 1279119721, //Argentina
-  CL: 1279119121, //Chile
-  GT: 1279118671, //Guatemala
-  RO: 1279117071, //Romania
-  SK: 1266973701, //Slovakia
-  CS: 1266972981, //Serbia
-  PL: 1266972311, //Poland
-  NL: 1266971851, //Netherlands
-  HR: 1266971131, //Croatia
-  CZ: 1266969571, //CzechRepublic
-  BE: 1266968331, //Belgium
-  LV: 1221037511, //Latvia
-  LT: 1221037371, //Lithuania
-  EE: 1221037201, //Estonia
-  FI: 1221034071, //Findland
-  HN: 1116190301, //Honduras
-  ES: 1116190041, //Spain
-  RU: 1116189381, //Russia
-  TR: 1116189071, //Turkey
-  ID: 1116188761, //Indonesia
-  CO: 1116188451, //Colombia
-  IT: 1116187241, //Italy
-  MX: 1111142361  //Mexico
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
 }
-///https://www.deezer.com/es/channels/module/6956d235-bf37-4e04-a7b4-92554f259fc9
+
+const urlParams = getUrlVars();
+//const myParam = urlParams.get('artist');
+
+var cabecera = document.querySelector('.main');
+
+if(urlParams.hasOwnProperty("q")){
+  var miBusqueda = urlParams["q"];
+  creaCabecera(cabecera,"topCuatroCirculos", "Resultados de ");
+  var URLBusqueda ="https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q="+miBusqueda+"&limit=50";
+  
+  var titulo = document.querySelector('#topOf');
+  titulo.insertAdjacentHTML('beforeend', miBusqueda);
+  
+  peticionAjax(URLBusqueda, function(respuesta) {
+    var topTracks = JSON.parse(respuesta);
+    console.log(topTracks);
+    var tracks = topTracks.data;
+    tracks.forEach(añadeATabla);
+    for(var i = 0; i < 4; i++){
+      ponEnCirculos(tracks[i], i);
+    }
+    console.log(tracks);
+   
+  });
+}else if(urlParams.hasOwnProperty("artist")){
+  var myParam = urlParams["artist"];
+  creaCabecera(cabecera,"artista");
+  peticionAjax("https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/"+myParam, pintaArtist);
+  peticionAjax("https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/"+myParam+"/top?limit=50", topTracksArtist);
+}else{
+  creaCabecera(cabecera,"topCuatroCirculos", "Top Tracks of ");
+  peticionAjax("https://ipapi.co/json/", topTracksCountry);
+}
+
+function creaCabecera(cabecera, tipo, titulo){
+  if (tipo === "topCuatroCirculos") {
+    cabecera.insertAdjacentHTML('afterbegin', `
+          <h1 class="page-header" id = "topOf">${titulo}</h1>
+
+          <div class="row placeholders">
+            <a class="col-xs-6 col-sm-3 placeholder" id="circulo-0">
+              <img id = "imagen-1" src="/public/resources/images/prueba.png" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </a>
+            <a class="col-xs-6 col-sm-3 placeholder" id="circulo-1">
+              <img id = "imagen-2" src="/public/resources/images/prueba.png" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </a>
+            <a class="col-xs-6 col-sm-3 placeholder" id="circulo-2">
+              <img id = "imagen-3" src="/public/resources/images/prueba.png" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </a>
+            <a class="col-xs-6 col-sm-3 placeholder" id="circulo-3">
+              <img id = "imagen-4" src="/public/resources/images/prueba.png" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </a>
+          </div>`);
+  }else if (tipo === "artista") {
+    cabecera.insertAdjacentHTML('afterbegin', `
+          <div class="row placeholdersArtist">
+            <div class="col-xs-3 col-sm-3 placeholderArtist" id="circulo">
+              <img id = "imagen-1" src="/public/resources/images/prueba.png" width="250" height="250" class="img-responsive" alt="Generic placeholder thumbnail">
+            </div> 
+            <div class="col-xs-7 col-sm-7 placeholderArtist"><h1>Label</h1>
+              <h5 class="text-muted nFans">Something else</h5>
+            </div>`);
+  }
+}
 
 function peticionAjax(url, callback) {
     var xmlHttp = new XMLHttpRequest();
@@ -90,7 +102,17 @@ function añadeATabla(item,index){
 
   var tr = document.createElement('tr');
   
-  tr.innerHTML = '<td># '+(index+1)+'</td> <td>' + item.title+'</td> <td>'+ item.artist.name +'</td> <td>'+ item.album.title +'</td>  <td><a href="'+ item.link +'" class = "playMe"> <img src="/public/resources/images/play.png" class = "playButton" alt="Play me"/>Play Me</a></td>'
+  var artistaTabla = "";
+  if(!myParam){
+    artistaTabla = '<a href="?artist='+ item.artist.id +'" class = "">'+ item.artist.name +'</a>';
+  }else{
+    for(var i=0; i<item.contributors.length-1; i++){
+      artistaTabla += '<a href="?artist='+ item.contributors[i].id +'" class = "">'+ item.contributors[i].name +', </a>';
+    }
+     artistaTabla += '<a href="?artist='+ item.contributors[item.contributors.length-1].id +'" class = "">'+ item.contributors[item.contributors.length-1].name +'</a>';
+  }
+  
+  tr.innerHTML = '<td># '+(index+1)+'</td> <td>' + item.title+'</td> <td>'+ artistaTabla +'</td> <td>'+ item.album.title +'</td>  <td><a href="'+ item.link +'" class = "playMe"> <img src="/public/resources/images/play.png" class = "playButton" alt="Play me"/>Play Me</a></td>';
 
   tabla.appendChild(tr);
 }
@@ -100,11 +122,26 @@ function ponEnCirculos(track,posicion){
   document.querySelector(circulo).setAttribute('href', track.link);
   document.querySelector(circulo +' > img').src = track.album.cover_xl;
   document.querySelector(circulo +' > h4').innerText = track.title;
-  document.querySelector(circulo +' > span').innerText = track.album.title;
+  document.querySelector(circulo +' > span').innerText = track.artist.name;
+}
+function pintaArtist(respuesta){
+  var artista = JSON.parse(respuesta);
+  document.querySelector('.placeholderArtist > h1').innerText = artista.name;
+  document.querySelector('.placeholderArtist > .nFans').innerText = artista.nb_fan + " fans";
+  document.querySelector('.placeholderArtist > img').src = artista.picture_xl;
+  
+}
+
+function topTracksArtist(respuesta){
+  var topTracks = JSON.parse(respuesta);
+  var tracks = topTracks.data;
+  tracks.forEach(añadeATabla);
+  
+  console.log(topTracks);
 }
 
 
-peticionAjax("https://ipapi.co/json/", function(respuesta) {
+function topTracksCountry(respuesta) {
   
   var localizacion = JSON.parse(respuesta);
   var pais = localizacion.country;
@@ -117,7 +154,7 @@ peticionAjax("https://ipapi.co/json/", function(respuesta) {
     titulo.insertAdjacentHTML('beforeend', localizacion.country_name);
   }else{
     idPlaylistPais = identificadoresPais.Mundial;
-    titulo.insertAdjacentHTML('beforeend', "Worldwide");
+    
   }
   var listaPais = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/" + idPlaylistPais + "?limit=50";
   
@@ -132,9 +169,4 @@ peticionAjax("https://ipapi.co/json/", function(respuesta) {
     console.log(tracks);
    
   });
-});
-
-
-
-//https://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=spain&api_key=69482ec81a0686a94e58bfedd7221697
-//ttps://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=spain&api_key=69482ec81a0686a94e58bfedd7221697&format=json
+}
